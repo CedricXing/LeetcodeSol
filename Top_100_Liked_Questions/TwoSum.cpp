f92787635695382b87dae59d74c993457cf03373
Cedric Xing <cedricxing0513@gmail.com>
2019-01-07 21:23:45 +0800

    Update TwoSum.cpp

--- Top_100_Liked_Questions/TwoSum.cpp
+++ Top_100_Liked_Questions/TwoSum.cpp
@@ -1,1 +1,36 @@
-
+class Solution {
+public:
+    /* Sol 0
+     * Brute force O(n^2)
+     * Two loops
+    */
+    
+    /* Sol 1
+     * Sort in O(nlogn)
+     * For each nums[i], find target - nums[i] by binary search --> O(nlogn)
+     * Totally, O(nlogn), better than Brute force
+    */
+  
+    /* Sol 2
+     * Use hash to find target - nums[i] in O(1)
+     * Conventionally, we need traverse the list twice, one for hashing and the other for searching.
+     * TRICK: hash target - nums[i] instead. So, if nums[i] is already in map, it means we find the result. Only traverse the list once.
+     * Final Code:
+    */
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_map<int,int> map;
+        for(int i = 0;i < nums.size();++i){
+            //target - nums[i] did not appear yet
+            if(map.find(nums[i]) == map.end()){
+                map[target - nums[i]] = i;
+            }
+            else{//find the solution
+                result.push_back(map[nums[i]]);
+                result.push_back(i);
+                break;//according to the assumpiton
+            }
+        }
+        return result;
+    }
+};
